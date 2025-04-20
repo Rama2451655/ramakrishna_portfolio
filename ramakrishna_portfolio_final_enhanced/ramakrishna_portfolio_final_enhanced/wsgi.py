5b958d700127546ebef5939f889b45ab8170ba6e
Rama2451655 <113767988+Rama2451655@users.noreply.github.com>
2025-04-20 12:49:27 +0530

    Delete ramakrishna_portfolio_final_enhanced directory

--- ramakrishna_portfolio_final_enhanced/ramakrishna_portfolio_final_enhanced/wsgi.py
+++
@@ -1,5 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ramakrishna_portfolio_final_enhanced.settings')
-application = get_wsgi_application()
